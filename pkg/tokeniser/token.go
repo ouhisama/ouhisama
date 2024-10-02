891adb06f802ea9c56f3a8ebf9961cfb29ac4026
uynilo9 <uynilo9@outlook.com>
2024-10-02 18:23:19 +0800

    Add some basic tokens to the tokeniser

--- pkg/tokeniser/token.go
+++ pkg/tokeniser/token.go
@@ -1,1 +1,97 @@
 package tokeniser
+
+import "fmt"
+
+type TokenKind uint
+
+const (
+	EOF TokenKind = iota
+	Newline
+	Whitespace
+	Indentation
+
+	Identifier
+	Number
+	String
+
+	Equal
+
+	Plus
+	Hyphen
+	Star
+	Slash
+	Percent
+
+	LBraket
+	RBraket
+)
+
+func (kind TokenKind) String() string {
+	switch kind {
+	case EOF:
+		return "EOF"
+	case Newline:
+		return "Newline"
+	case Whitespace:
+		return "Whitespace"
+	case Indentation:
+		return "Indentation"
+	case Identifier:
+		return "Identifier"
+	case Number:
+		return "Number"
+	case String:
+		return "String"
+	case Equal:
+		return "Equal"
+	case Plus:
+		return "Plus"
+	case Hyphen:
+		return "Hyphen"
+	case Star:
+		return "Star"
+	case Slash:
+		return "Slash"
+	case Percent:
+		return "Percent"
+	case LBraket:
+		return "LBracket"
+	case RBraket:
+		return "RBracket"
+	default:
+		return "Unknown"
+	}
+}
+
+type TokenValue string
+
+type Token struct {
+	Kind  TokenKind
+	Value TokenValue
+}
+
+func (token Token) isOneOf(kinds ...TokenKind) bool {
+	for _, kind := range kinds {
+		if token.Kind == kind {
+			return true
+		}
+	}
+	return false
+}
+
+func (token Token) Debug() {
+	if token.isOneOf(Identifier, Number, String) {
+		fmt.Printf("%v: \"%v\"\n", token.Kind.String(), token.Value)
+	} else {
+		fmt.Printf("%v: \"\"\n", token.Kind.String())
+	}
+}
+
+type TokenList []Token
+
+func New(kind TokenKind, value TokenValue) Token {
+	return Token{
+		Kind:  kind,
+		Value: value,
+	}
+}
--- pkg/tokeniser/token_test.go
+++ pkg/tokeniser/token_test.go
@@ -1,1 +1,95 @@
 package tokeniser
+
+import "testing"
+
+var tokens = TokenList{
+	New(EOF, "\x00"),
+	New(Newline, "\n"),
+	New(Whitespace, " "),
+	New(Indentation, "\t"),
+	New(Identifier, "uynilo9"),
+	New(Number, "213"),
+	New(String, "hello world"),
+	New(Equal, "="),
+	New(Plus, "+"),
+	New(Hyphen, "-"),
+	New(Star, "*"),
+	New(Slash, "/"),
+	New(Percent, "%"),
+	New(LBraket, "("),
+	New(RBraket, ")"),
+}
+
+func TestTokenKindString(t *testing.T) {
+	want := []string{
+		"EOF",
+		"Newline",
+		"Whitespace",
+		"Indentation",
+		"Identifier",
+		"Number",
+		"String",
+		"Equal",
+		"Plus",
+		"Hyphen",
+		"Star",
+		"Slash",
+		"Percent",
+		"LBracket",
+		"RBracket",
+	}
+	for i, token := range tokens {
+		if token.Kind.String() != want[i] {
+			t.Fatalf("ERROR Expected `%v`, got `%v`", want[i], token.Kind.String())
+		}
+	}
+	t.Logf("SUCCESS All passed")
+}
+
+func TestTokenDebug(t *testing.T) {
+	for _, token := range tokens {
+		token.Debug()
+	}
+	// Output:
+	// EOF: ""
+	// Newline: ""
+	// Whitespace: ""
+	// Indentation: ""
+	// Identifier: "uynilo9"
+	// Number: "213"
+	// String: "hello world"
+	// Equal: ""
+	// Plus: ""
+	// Hyphen: ""
+	// Star: ""
+	// Slash: ""
+	// Percent: ""
+	// LBracket: ""
+	// RBracket: ""
+}
+
+func TestNew(t *testing.T) {
+	want := TokenList{
+		{Kind: EOF, Value: "\x00"},
+		{Kind: Newline, Value: "\n"},
+		{Kind: Whitespace, Value: " "},
+		{Kind: Indentation, Value: "\t"},
+		{Kind: Identifier, Value: "uynilo9"},
+		{Kind: Number, Value: "213"},
+		{Kind: String, Value: "hello world"},
+		{Kind: Equal, Value: "="},
+		{Kind: Plus, Value: "+"},
+		{Kind: Hyphen, Value: "-"},
+		{Kind: Star, Value: "*"},
+		{Kind: Slash, Value: "/"},
+		{Kind: Percent, Value: "%"},
+		{Kind: LBraket, Value: "("},
+		{Kind: RBraket, Value: ")"},
+	}
+	for i, token := range tokens {
+		if token != want[i] {
+			t.Fatalf("ERROR Expected `%v: \"%v\"`, got `%v \"%v\"`", want[i].Kind.String(), want[i].Value, token.Kind.String(), token.Value)
+		}
+	}
+	t.Logf("SUCCESS All passed")
+}
