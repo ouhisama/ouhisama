a4ae41770ecbe63bd8ce5114c2ca1230477622fe
uynilo9 <uynilo9@outlook.com>
2024-10-09 19:50:15 +0800

    Modify some error messages

--- cmd/ouhisama/ouhisama.go
+++ cmd/ouhisama/ouhisama.go
@@ -12,17 +12,17 @@ import (
 func main() {
 	args := os.Args
 	if len(args) != 2 {
-		log.Fatalf("ERROR: Expected 1 argument, but got %v\n", len(args))
+		log.Fatalf("ERROR Expected 1 argument, but got %v\n", len(args))
 	}
 
 	path, err := filepath.Abs(args[1])
 	if err != nil {
-		log.Fatalf("ERROR: Failed to get absolute path of file `%v`\n", args[1])
+		log.Fatalf("ERROR Failed to get absolute path of file `%v`\n", args[1])
 	}
 
 	source, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatalf("ERROR: Failed to read file `%v`\n", path)
+		log.Fatalf("ERROR Failed to read file `%v`\n", path)
 	}
 
 	tokens := tokeniser.Tokenise(string(source))
